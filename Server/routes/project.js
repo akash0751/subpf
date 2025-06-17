071ab99f08f23100d0b1e046be07364214ccee67
akash0751 <akashsofficial05@gmail.com>
2025-06-17 12:51:32 +0530

    Update socket connection to use live backend

--- Server/routes/project.js
+++ Server/routes/project.js
@@ -3,10 +3,10 @@ const router = express.Router();
 const {createProject,deleteProject,getProject,getProjectById,updateProject} = require('../contoller/project')
 const authMiddleware = require('../middleware/userAuth')
 const apiKeyAuth = require('../middleware/apiKeyAuth')
-router.post('/add',authMiddleware,createProject)
-router.delete('/delete/:id',authMiddleware,deleteProject)
+router.post('/add',apiKeyAuth,createProject)
+router.delete('/delete/:id',apiKeyAuth,deleteProject)
 router.get('/get',apiKeyAuth,getProject)
 router.get('/get/:UserId',getProjectById)
-router.put('/update/:id',authMiddleware,updateProject)
+router.put('/update/:id',apiKeyAuth,updateProject)
 
 module.exports = router;
